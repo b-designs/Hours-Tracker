0ff00c0db8b05ba230c690282bca1012dd5d8887
boz <blopezdesigns@gmail.com>
2024-02-23 00:55:33 -0800

    CSS updated for buttons and add infobox section

--- hours-tracker/src/App.js
+++ hours-tracker/src/App.js
@@ -2,12 +2,27 @@
 import './App.css';
 import Button from './Button';
 
+onclick="startButton()"
+
 
 function App() {
   return (
     <>
       <div>
         <Button name="Start"/>
+      </div>
+      <div>
+        <Button name="Stop"/>
+      </div>
+      <div class="infoBox">
+        <text id="info">Info</text>
+        <text id="in">In</text>
+        <text id="out">Out</text>
+      </div>
+      <div>
+        <text id="infoLog"></text>
+        <text id="inLog"></text>
+        <text id="outLog"></text>
       </div>
     </>
   );
--- hours-tracker/src/Button.js
+++ hours-tracker/src/Button.js
@@ -1,6 +1,10 @@
 
 import './App.css';
 
+// Currently not working...wondering why?
+function startButton(){
+  console.log("startButton function works1!");
+}
 
 function Button(props) {
     return (
